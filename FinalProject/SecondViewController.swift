a2ea18c70720252f43be27a7223f1a5d0b944505
Sophia <sophiabateman@icloud.com>
2022-08-17 20:21:38 +0100

    Initial Commit

---
+++ FinalProject/SecondViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SecondViewController.swift
+//  FinalProject
+//
+//  Created by Sophia Bateman on 17/08/2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    
+    @IBOutlet weak var background2: UIImageView!
+    
+    @IBOutlet weak var presentedByLabel2: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Sophia Bateman on 17/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var background1: UIImageView!
+    
+    @IBOutlet weak var alchemyLabel: UILabel!
+    
+    @IBOutlet weak var newsLabel: UILabel!
+    
+    
+    @IBOutlet weak var presentedByLabel1: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
