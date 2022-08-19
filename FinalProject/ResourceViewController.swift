84796e778333a19a3347c7dbedb1561460671a6c
Kibali O'Donald <kibali.odonald@gmail.com>
2022-08-19 10:44:27 -0400

    new code

---
+++ FinalProject/ResourceViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ResourceViewController.swift
+//  FinalProject
+//
+//  Created by Kibali O’Donald on 8/18/22.
+//
+
+import UIKit
+
+class ResourceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    
+}
